55e22b764b60597db339fc40be56c6eb79ecdd42
NamanMathur <36097162+NamanMathur77@users.noreply.github.com>
2019-02-02 12:40:48 +0530

    initial commit

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ var hambtn=document.getElementById("ham");
 function myFunction(x) {
   x.classList.toggle("change");
 }
+
+function on() {
+  $("#overlay").fadeIn("slow");
+}
+function off() {
+  $("#overlay").fadeOut("slow");
+  hambtn.classList.toggle("change");
+}
