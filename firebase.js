ffaded42cf0c8fe51caf86caeba69910b614e3b2
NamanMathur77 <namanmathur619@gmail.com>
2019-02-22 22:03:08 +0530

    adding firebase

---
+++ firebase.js
@@ -0,0 +1,12 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDKwZMXXtpkbGLKjNIJgVYZ3TPXX4CjB6A",
+    authDomain: "ignite-60d9e.firebaseapp.com",
+    databaseURL: "https://ignite-60d9e.firebaseio.com",
+    projectId: "ignite-60d9e",
+    storageBucket: "ignite-60d9e.appspot.com",
+    messagingSenderId: "941173824400"
+  };
+  firebase.initializeApp(config);
+</script>
